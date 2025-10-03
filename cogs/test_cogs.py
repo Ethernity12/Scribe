12db1d6d133cfc3723583c26681ef00183e6888e
Vladyslav <vladyslavsavchuk2004@gmail.com>
2025-10-03 23:43:09 +0300

    Created decorator for logging executed commands

--- cogs/test_cogs.py
+++ cogs/test_cogs.py
@@ -1,9 +1,11 @@
 from disnake.ext import commands
+from ext.logger import command_logger
 
 class ExampleCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.slash_command()
+    @command_logger()
     async def ping(self, inter):
         await inter.response.send_message("Pong!")
--- ext/logger.py
+++ ext/logger.py
@@ -1,6 +1,11 @@
 import logging
+import functools
+import disnake
+import time
+from pathlib import Path
 
-def setup_logger(name: str, log_file: str, level=logging.INFO):
+
+def setup_logger(name: str, level=logging.INFO):
     logger = logging.getLogger(name)
     logger.setLevel(level)
 
@@ -13,10 +18,39 @@ def setup_logger(name: str, log_file: str, level=logging.INFO):
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
 
-    file_handler = logging.FileHandler(log_file, encoding="utf-8")
+    file_handler = logging.FileHandler(Path('logs', name + '.log'), encoding="utf-8")
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
 
     return logger
 
-main_logger = setup_logger("main", "main.log")
+main_logger = setup_logger("main")
+
+def command_logger():
+    def decorator(func):
+        logger = setup_logger(func.__module__)
+        @functools.wraps(func)
+        async def wrapper(self, inter: disnake.ApplicationCommandInteraction, *args, **kwargs):
+            start = time.perf_counter()
+            try:
+                result = await func(self, inter, *args, **kwargs)
+
+                duration = (time.perf_counter() - start) * 1000
+                logger.info(
+                    f"[SUCCESS] /{func.__name__} by {inter.author} "
+                    f"in {inter.guild.name if inter.guild else 'DM'} "
+                    f"Duration: {duration:.2f} ms"
+                )
+                return result
+
+            except Exception as e:
+                duration = (time.perf_counter() - start) * 1000
+                logger.error(
+                    f"[FAILED] /{func.__name__} by {inter.author} "
+                    f"in {inter.guild.name if inter.guild else 'DM'} "
+                    f"Error: {e} "
+                    f"Duration: {duration:.2f} ms"
+                )
+                raise
+        return wrapper
+    return decorator           
